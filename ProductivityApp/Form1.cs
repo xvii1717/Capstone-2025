3fe4cb0c60674c2aaf538bfef986a4cc4f620265
Thomas Santonicola <thomas.santonicola@gmail.com>
2025-08-22 12:47:55 -0400

    Project Initial Setup

---
+++ ProductivityApp/Form1.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace ProductivityApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
