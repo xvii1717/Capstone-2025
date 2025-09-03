519bea19c9f1d92f6fa8c56be266caa00c4e7063
Thomas Santonicola <thomas.santonicola@gmail.com>
2025-09-03 12:05:19 -0400

    more features

--- ProductivityApp/obj/Debug/net6.0-windows/ProductivityApp.AssemblyInfo.cs
+++ ProductivityApp/obj/Debug/net6.0-windows/ProductivityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductivityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f2ca6e021460724e609042ea79150ba50fcf74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0823510c47825318e466aad9eaedebfc0fc6d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductivityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductivityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
